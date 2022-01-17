e6a85dfaf65d5278f1cc2972352357532aadc2ec
Anton Rybianov <apollo301269@gmail.com>
2022-01-17 12:45:33 +0300

    build: fix cmake files

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,43 +1,52 @@
 include(FetchContent)
 
-function(bench_add_library LIBRARY)
-    if (NOT ${DOWNLOAD_DEPENDENCIES})
-        find_package(${LIBRARY} REQUIRED)
-        return()
-    endif ()
-
-    message(STATUS "Trying to find library ${LIBRARY}")
-    find_package(${LIBRARY} QUIET)
-    if (${LIBRARY}_Found)
-        message(STATUS "Found ${LIBRARY}")
-    else ()
-        message(STATUS "${LIBRARY} was not found. Fetching.")
-        FetchContent_Declare(
-                ${LIBRARY}_fetch
-                GIT_REPOSITORY https://github.com/google/googletest.git
-                GIT_TAG "release-1.11.0"
-        )
-        FetchContent_MakeAvailable(${LIBRARY}_fetch)
-    endif ()
-endfunction()
-
 # ------------------------------------------------------------------------------
 # Google Test
 # ------------------------------------------------------------------------------
 if (${ENABLE_TESTS})
-    # For Windows: Prevent overriding the parent project's compiler/linker settings
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-    bench_add_library(GTest)
+    if (NOT ${DOWNLOAD_DEPENDENCIES})
+        find_package(GTest REQUIRED)
+    else ()
+        find_package(GTest QUIET)
+        if (NOT GTest_Found)
+            message(STATUS "GTest was not found. Fetching.")
+            FetchContent_Declare(
+                    ${LIBRARY}_fetch
+                    GIT_REPOSITORY https://github.com/google/googletest.git
+                    GIT_TAG "release-1.11.0"
+            )
+            # For Windows: Prevent overriding the parent project's
+            # compiler/linker settings
+            set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+            FetchContent_MakeAvailable(GTest_fetch)
+        endif ()
+    endif ()
 endif ()
 
 # ------------------------------------------------------------------------------
 # Google benchmark
 # ------------------------------------------------------------------------------
 
+if (NOT ${DOWNLOAD_DEPENDENCIES})
+    find_package(benchmark REQUIRED)
+else ()
+    find_package(benchmark QUIET)
+    if (NOT benchmark_Found)
+        message(STATUS "benchmark was not found. Fetching.")
 
-set(BENCHMARK_ENABLE_INSTALL OFF)
-set(BENCHMARK_ENABLE_TESTING OFF)
+        set(BENCHMARK_ENABLE_INSTALL OFF)
+        set(BENCHMARK_ENABLE_TESTING OFF)
 
-bench_add_library(benchmark)
+        FetchContent_Declare(
+                benchmark_fetch
+                GIT_REPOSITORY https://github.com/google/benchmark.git
+                GIT_TAG "v1.6.0"
+        )
+        # For Windows: Prevent overriding the parent project's
+        # compiler/linker settings
+        set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+        FetchContent_MakeAvailable(benchmark_fetch)
+    endif ()
+endif ()
 
 # ------------------------------------------------------------------------------
